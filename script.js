81d053cbc572eec58096aeb11867cad9c22238d5
Thiago H. Hormann <thiago.h.hormann@gmail.com>
2023-03-27 17:07:53 -0300

    Finalizar função para receber e mostrar de forma dinâmica os dados da API

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
-const chaveApi="f87f9bafe1b3b5d2fcf29e6edce21f98";
-const urlApi="https://api.openweathermap.org/data/2.5/weather?units=metric&q=Joinville";
+const barraDePesquisa = document.getElementById("barra-pesquisa");
+const btnPesquisa = document.getElementById("btn-pesquisa");
 
-async function checarTempo(){
-    const resposta = await fetch(urlApi + `&appid=${chaveApi}`);
-    var data = await resposta.json();
+const cidadeElemento = document.querySelector(".cidade");
+const temperaturaElemento = document.querySelector(".temperatura");
+const umidadeElemento = document.querySelector(".umidade");
+const ventoElemento = document.querySelector(".vento");
 
-    console.log(data);
+async function recebeDadosDoClima(cidade) {
+  const chaveApi = "f87f9bafe1b3b5d2fcf29e6edce21f98";
+  const urlApi =
+    "https://api.openweathermap.org/data/2.5/weather?units=metric&lang=pt_br";
 
-    temperatura = data.main.temp;
+  const resposta = await fetch(`${urlApi}&appid=${chaveApi}&q=${cidade}`);
+  let data = await resposta.json();
 
-    temperaturaInt = Math.trunc(temperatura);
-    temperaturaFloat = temperatura - temperaturaInt;
-
-    if (temperaturaFloat<=0.95 && temperaturaFloat >= 0.1){
-        temperatura = temperatura.toFixed(1);
-    } else if (temperaturaFloat < 0.1) {
-        temperatura = temperatura.toFixed(0);
-    }
-
-
-    document.querySelector(".cidade").innerHTML = data.name;
-    document.querySelector(".temperatura").innerHTML = temperatura + "°C";
-    document.querySelector(".umidade").innerHTML = data.main.humidity + "%";
-    document.querySelector(".vento").innerHTML = data.wind.speed + " km/h";
-
+  return data;
 }
 
-checarTempo();
+async function mostrarDadosDoClima(cidade) {
+  const data = await recebeDadosDoClima(cidade);
+
+  console.log(data);
+
+  temperatura = data.main.temp;
+  temperaturaInt = Math.trunc(temperatura);
+  temperaturaFloat = temperatura - temperaturaInt;
+
+  if (temperaturaFloat <= 0.95 && temperaturaFloat >= 0.1) {
+    temperatura = temperatura.toFixed(1);
+  } else if (temperaturaFloat < 0.1) {
+    temperatura = temperatura.toFixed(0);
+  }
+
+  umidade = data.main.humidity;
+  velocidadeVento = data.wind.speed;
+
+  document.querySelector(".cidade").textContent = data.name;
+  document.querySelector(".temperatura").textContent = temperatura + "°C";
+  document.querySelector(".umidade").textContent = data.main.humidity + "%";
+  document.querySelector(".vento").textContent = data.wind.speed + " km/h";
+}
+
+btnPesquisa.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  cidade = barraDePesquisa.value;
+
+  mostrarDadosDoClima(cidade);
+});
