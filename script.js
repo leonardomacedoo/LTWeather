df1806990185eb6fb86505c242181315ce086a7e
Thiago H. Hormann <thiago.h.hormann@gmail.com>
2023-03-28 21:16:13 -0300

    Make the weather icon dynamic

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const barraDePesquisa = document.getElementById("barra-pesquisa");
 const btnPesquisa = document.getElementById("btn-pesquisa");
 
 const cidadeElemento = document.querySelector(".cidade");
+const iconeElemento = document.querySelector(".icone-tempo");
 const temperaturaElemento = document.querySelector(".temperatura");
 const umidadeElemento = document.querySelector(".umidade");
 const ventoElemento = document.querySelector(".vento");
 
 const card = document.querySelector(".card");
 const tempo = document.querySelector(".tempo");
-
 
 async function recebeDadosDoClima(cidade) {
   const chaveApi = "f87f9bafe1b3b5d2fcf29e6edce21f98";
@@ -18,10 +18,42 @@ async function recebeDadosDoClima(cidade) {
   try {
     const resposta = await fetch(`${urlApi}&appid=${chaveApi}&q=${cidade}`);
     let data = await resposta.json();
+    console.log(data.weather[0].id, data);
     return data;
-  } catch (error){
+  } catch (error) {
     recebeDadosDoClima(cidade);
   }
+}
+
+function definirIconeClima(infoClima) {
+  let diaOuNoite = infoClima.icon.endsWith("n") ? `night-` : "";
+
+  if (infoClima.id < 300) {
+    caminhoIcone = `imagens/${diaOuNoite}storm.png`;
+  }
+  if (infoClima.id < 400) {
+    caminhoIcone = `imagens/${diaOuNoite}drizzle.png`;
+  }
+  if (infoClima.id < 500) {
+    caminhoIcone = `imagens/rain.png`;
+  }
+  if (infoClima.id < 600) {
+    caminhoIcone = `imagens/snow.png`;
+  }
+  if (infoClima.id < 700) {
+    caminhoIcone = `imagens/${diaOuNoite}storm.png`;
+  }
+  if (infoClima.id < 800) {
+    caminhoIcone = `imagens/mist.png`;
+  }
+  if (infoClima.id == 800) {
+    caminhoIcone = `imagens/${diaOuNoite}clear.png`;
+  }
+  if (infoClima.id > 800) {
+    caminhoIcone = `imagens/${diaOuNoite}clouds.png`;
+  }
+
+  return caminhoIcone;
 }
 
 async function mostrarDadosDoClima(cidade) {
@@ -39,13 +71,16 @@ async function mostrarDadosDoClima(cidade) {
     temperatura = temperatura.toFixed(0);
   }
 
+  const caminhoIcone = definirIconeClima(data.weather[0]);
+
   umidade = data.main.humidity;
   velocidadeVento = data.wind.speed;
 
-  document.querySelector(".cidade").textContent = data.name;
-  document.querySelector(".temperatura").textContent = temperatura + "°C";
-  document.querySelector(".umidade").textContent = data.main.humidity + "%";
-  document.querySelector(".vento").textContent = data.wind.speed + " km/h";
+  cidadeElemento.textContent = data.name;
+  iconeElemento.setAttribute("src", caminhoIcone);
+  temperaturaElemento.textContent = temperatura + "°C";
+  umidadeElemento.textContent = data.main.humidity + "%";
+  ventoElemento.textContent = data.wind.speed + " km/h";
 }
 
 btnPesquisa.addEventListener("click", (e) => {
@@ -53,13 +88,13 @@ btnPesquisa.addEventListener("click", (e) => {
 
   cidade = barraDePesquisa.value;
 
-  if(!cidade){
+  if (!cidade) {
     alert("Adicione o nome da cidade");
     return;
   }
 
-  if(!card.classList.contains('grande')) {
-   card.classList.toggle('grande');
+  if (!card.classList.contains("grande")) {
+    card.classList.toggle("grande");
   }
 
   mostrarDadosDoClima(cidade);
